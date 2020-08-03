33986e59e0f97a899f1c8f7d06337c17a71dee15
FedericaC03 <fdrk03@hotmail.it>
2020-08-03 14:32:14 +0200

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-alert('ciao')
+// INSERISCO LA VARIABILE PER IL NOME
+var nome = prompt('Scrivi il tuo nome');
+
+// INSERISCO LA VARIABILE PER IL COGNOME
+var cognome = prompt('Scrivi il tuo cognome');
+
+// INSERISCO LA VARIABILE PER IL COLORE
+var colore = prompt('Scrivi il tuo colore preferito');
+
+//PASSWORD GENERATOR
+
+var password = nome + cognome + colore + "19";
+document.getElementById('password').innerHTML = password;
+
+alert('ciao Eli, ciao Enri')
